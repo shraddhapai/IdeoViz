b4a6be211c6412a7e1cf34c496f33d41bc0669bb
Shraddha Pai <Shraddha.Pai@camh.ca>
2016-12-29 15:01:10 +0000

    Added new data viz type, seg_tracks, for tracks of ranges
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IdeoViz@125503 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getIdeo.R
+++ R/getIdeo.R
@@ -1,3 +1,20 @@
+#' Download ideogram table from UCSC
+#' 
+#' @details Download table containing chromosomal extent and band locations from the UCSC genome browser
+#' Uses \code{rtracklayer} to retrieve the \emph{cytoBandIdeo}.
+#' table from the UCSC genome browser. The \emph{cytoBandIdeo} table  
+#' contains chromosomal ideogram information and is used to graph the 
+#' chromosomal bands in \code{plotOnIdeo()}. This table is provided as 
+#' input to \code{plotOnIdeo()}. In the case where the user bins the 
+#' data, the output of this function can also be used as input to 
+#' generate bin coordinates for binning the data (see 
+#' \code{avgByBin()}). 
+#' @usage getIdeo(ideoSource)
+#' @param ideoSource (character) Genome build for data (e.g. mm10).
+#' @return (data.frame) ideogram table
+#' @examples getIdeo("mm9")
+#' @seealso \code{avgByBin()},\code{getBins()}
+#' @export
 getIdeo <- function(ideoSource) {
     cytoTable <- NULL
     
--- R/plotChromValuePair.R
+++ R/plotChromValuePair.R
@@ -1,7 +1,50 @@
-plotChromValuePair <- function(chrom,cytoTable,bpLim,vertical,values_GR,
-val_range,col,value_cols='values',default_margins,addScale,ablines_y,
-smoothVals,span=0.03,verbose=FALSE,...)
-{
+#' Base function which plots the ideogram and superimposed data for a single chromosome. plotOnIdeo() calls this function and stacks the resulting output.
+#'
+#' @details Plots one unit of chromosome ideogram with dataseries 
+#' superimposed. Usually, the user can avoid this function and directly
+#' call plotOnIdeo(). However, this function may be used in 
+#' cases where further plot customization is required.
+#' @usage plotChromValuePair(chrom, cytoTable, bpLim, vertical, 
+#' values_GR,val_range, col, value_cols = "values", default_margins, 
+#' addScale, ablines_y, smoothVals, span=0.03, verbose = FALSE, ...)
+#' @param chrom(character) chromosome(s) to create ideograms for 
+#' @param cytoTable(data.frame) loaded ideogram table. 
+#' (see ideoTable argument to plotOnIdeo())
+#' @param bpLim(numeric) (aka xlim); display only a section of the 
+#' chromosome and the corresponding values
+#' @param vertical(logical) if TRUE, chromosomes will be plotted 
+#' vertically
+#' @param values_GR(list or GenomicRanges) If plotType is "lines" or
+#' "rect", the function expects this to be a GRanges() object with 
+#' data series in metadata columns. 
+#' If plotType is "seg_tracks" this is a list of GRanges(), each 
+#' entry of which represents a track. 
+#' @param val_range(numeric) (aka ylim); y-axis scale for data series
+#' @param col(character) colour for series
+#' @param value_cols(character) column name for series to plot
+#' @param default_margins(numeric) page inner margins (in inches)
+#' @param addScale(logical) if FALSE, bp positions will be hidden
+#' @param ablines_y(numeric) when specified, will draw reference lines 
+#' on the y-axis
+#' @param smoothVals(logical) when T applies loess() to each series
+#' @param span(numeric) loess::span()
+#' @param verbose(logical) print messages
+#' @param ... arguments to \code{axis()}, \code{line()}, and 
+#' \code{rect()}
+#' @examples
+#' data(hg18_ideo)
+#' data(binned_multiSeries)
+#' layout(matrix(1:2, byrow=TRUE,ncol=1),heights=c(2.5,1))
+#' plotChromValuePair("chr1",hg18_ideo, 
+#' 	values_GR=binned_multiSeries,
+#'	value_cols=colnames(mcols(binned_multiSeries)),plotType='lines',
+#' 	col=1:5,val_range=c(0,10),bpLim=NULL,vertical=FALSE,addScale=TRUE,
+#'	ablines_y=NULL,smoothVals=FALSE,default_margins=c(0.5,.5,.1,.1))
+#' @seealso plotOnIdeo()
+#' @export
+plotChromValuePair <- function(chrom,cytoTable,bpLim,vertical,
+	values_GR,val_range,col,value_cols='values',default_margins,
+	addScale,ablines_y,smoothVals,span=0.03,verbose=FALSE,...) {
   ideo <- cytoTable[cytoTable$chrom==chrom,]
   chromStart <- min(ideo$chromStart)
   chromEnd <- max(ideo$chromEnd)
@@ -10,63 +53,100 @@ smoothVals,span=0.03,verbose=FALSE,...)
                chromStart=chromStart, chromEnd=chromEnd, 
                bpLim=bpLim, chrom=chrom,vertical=vertical,
                addScale=addScale, ...)
+  plotType <- args$plotType
+#print(plotType)
   if (any(grep("plotType",names(args)))) args[["plotType"]] <- NULL
   if (any(grep("xlab",names(args)))) args[["xlab"]] <- NULL
   if (any(grep("ylab",names(args)))) args[["ylab"]] <- NULL
   
-  plotValue_args <- list(bpLim=bpLim, chrom=chrom, 
-  	values_GR=values_GR[as.character(seqnames(values_GR))==chrom],
-    vertical=vertical, bty='n',val_range=val_range,ablines_y=ablines_y,
-	smoothVals=smoothVals,...) 
+  # limit to chroms being plotted
+  if (class(values_GR)=="list") {
+	values_GR <- lapply(values_GR, function(x) {
+		x[as.character(seqnames(x)) %in% chrom]
+	})
+  } else {
+	values_GR <- values_GR[as.character(seqnames(values_GR)) 
+	  %in% chrom]
+}
+  plotValue_args <- list(
+	bpLim=bpLim, chrom=chrom, 
+  	values_GR=values_GR,
+    vertical=vertical, bty='n',val_range=val_range,
+	ablines_y=ablines_y,smoothVals=smoothVals,...) 
   plotValue_args[["span"]] <- NULL
 
-    newPlot <- TRUE
-    if (vertical){   
-        curr_mai <- c(0,0,default_margins[3],0)
-        if (addScale) { curr_mai <- replace(curr_mai,2, 0.3) }
+  newPlot <- TRUE
+  if (vertical) {
+	curr_mai <- c(0,0,default_margins[3],0)
+	#curr_mai <- c(0,0,0,0)
+    if (addScale) curr_mai <- replace(curr_mai,2, 0.3)
         
         par(mai=curr_mai)
-        #cat("\tIdeo: "); .printMargins()
-        #browser()
         do.call(".plotChromosome", args)
+cat("Plot chromosome done\n")
         ctr <- 1
-        for (k in value_cols) {
-            if (verbose) cat(sprintf('\t%s\n',k))
-            
-            col_idx <- ctr %% length(col); 
-            if (col_idx==0) {col_idx=length(col)}
-            plotValue_args <- modifyList(plotValue_args, 
-            list(value_cols=k, newPlot=newPlot,
-            col=col[col_idx]))
-            par(mai=default_margins)
-            # cat("\tValue: "); .printMargins()
-            suppressWarnings(do.call('.plot_values',plotValue_args))
-            newPlot <- FALSE; ctr <- ctr+1
-        }
-    }
-else{
+
+		if (plotType %in% "seg_tracks") {
+			plotValue_args <- modifyList(plotValue_args, 
+	            	list(value_cols="", newPlot=TRUE,col=col,
+						 yaxt='n'))
+	            	par(mai=default_margins)
+	        # cat("\tValue: "); .printMargins()
+	        suppressWarnings(do.call('.plot_values',plotValue_args))
+		} else {
+			# plot each series in turn
+	        for (k in value_cols) {
+	            if (verbose) cat(sprintf('\t%s\n',k))
+	            
+	            col_idx <- ctr %% length(col); 
+	            if (col_idx==0) col_idx=length(col)
+
+	            plotValue_args <- modifyList(plotValue_args, 
+	            	list(value_cols=k, newPlot=newPlot,
+	            	col=col[col_idx]))
+	            	par(mai=default_margins)
+	            # cat("\tValue: "); .printMargins()
+	            suppressWarnings(
+					do.call('.plot_values',plotValue_args)
+				)
+	            newPlot <- FALSE; ctr <- ctr+1
+	        }
+		}
+ } else { # horizontal
     ctr <- 1
-    par(mai=default_margins)
-    # cat("\tValue: "); .printMargins()
-    
-    for (k in value_cols) {
-        if (verbose) cat(sprintf('\t%s\n',k))
-        col_idx <- ctr %% length(col); if (col_idx==0) {col_idx=length(col)}
-        plotValue_args <- modifyList(plotValue_args, 
-            list(value_cols=k, newPlot=newPlot,
-                col=col[col_idx]))
-        
-        suppressWarnings(do.call('.plot_values',plotValue_args))
-        newPlot <- FALSE; ctr <- ctr+1
-    }
+
+	if (plotType %in% "seg_tracks") {
+		plotValue_args <- modifyList(plotValue_args, 
+	           	list(value_cols="", newPlot=TRUE,yaxt='n',col=col))
+	           	par(mai=default_margins)
+	      #cat("\tValue: "); .printMargins()
+	    suppressWarnings(
+			do.call('.plot_values',plotValue_args)
+		)
+	} else { # lines,rect
+		par(mai=default_margins)
+     	#cat("\tValue: "); .printMargins()
+	    for (k in value_cols) {
+	        if (verbose) cat(sprintf('\t%s\n',k))
+	        col_idx <- ctr %% length(col); 
+			if (col_idx==0) col_idx=length(col)
+	
+	        plotValue_args <- modifyList(plotValue_args, 
+	            list(value_cols=k, newPlot=newPlot,
+	                col=col[col_idx]))
+	        
+	        suppressWarnings(do.call('.plot_values',plotValue_args))
+	        newPlot <- FALSE; ctr <- ctr+1
+	    }
+	}
     #abline(h=0.9,lty=3,col='black')
+    
     curr_mai <- c(0,default_margins[2],0,0)
     if (addScale) { curr_mai <- replace(curr_mai,1, 0.3) }
-    
     par(mai=curr_mai)
     # cat("\tIdeo after call: "); .printMargins()
     suppressWarnings(do.call(".plotChromosome", args))
-    }; 
+}; 
 }
 
 .plotChromosome <- function(chrom,ideo, chromStart, chromEnd,bpLim=NULL,
@@ -149,8 +229,8 @@ chromName_cex=0.8,verbose=FALSE,...)
     }; rm(col)
     # add chrom name
     if (vertical) {
-        mtext(chrom, side=titleVertical,line=2, outer=FALSE,col='gray50', 
-        adj=c(1,1),cex=chromName_cex,font=2)
+        mtext(chrom, side=titleVertical,line=2, outer=FALSE,
+			col='gray50',adj=c(1,1),cex=chromName_cex,font=2)
     } else {
         mtext(chrom, side=titleHorizontal,line=0.5,outer=FALSE, 
         col='gray50', cex=chromName_cex,font=2,las=3)
@@ -182,27 +262,40 @@ chromName_cex=0.8,verbose=FALSE,...)
 
 .plot_values <- function(chrom, bpLim,values_GR, vertical=TRUE, 
 plotType='rect', col='gray50',val_range=NULL,  value_cols="values", 
- ablines_y=NULL, newPlot=TRUE,smoothVals=FALSE,verbose=FALSE,...)
- {
-    #if (missing(col)) { col <- "gray50" } 
-    values_GR <- values_GR[as.character(seqnames(values_GR)) == chrom]
-    
-    cvalues <- mcols(values_GR)[,value_cols]
-    if (is.null(val_range)) { val_range <- c(min(cvalues,na.rm=TRUE),
-        max(cvalues, na.rm=TRUE)) }
-    rng_start <- start(ranges(values_GR))
-    rng_end <- end(ranges(values_GR))
+ ablines_y=NULL, newPlot=TRUE,smoothVals=FALSE,verbose=FALSE,...) {
+
+	# initialize plot limits
+	if (plotType %in% "seg_tracks") {
+		padding <- max(7-length(values_GR),1)
+		val_range 	<- c(0,length(values_GR)+padding)
+		rng_start 	<- start(ranges(values_GR[[1]]))
+		rng_end		<- end(ranges(values_GR[[1]])) 
+	} else {
+		idx 	<- which(as.character(seqnames(values_GR)) == chrom) 
+  		values_GR	<- values_GR[idx]
+    	cvalues 	<- mcols(values_GR)[,value_cols]
+    	if (is.null(val_range)) { 
+			val_range <- c(min(cvalues,na.rm=TRUE),
+						   max(cvalues,na.rm=TRUE)) 
+		}
+    	rng_start <- start(ranges(values_GR))
+    	rng_end <- end(ranges(values_GR))
+	} 
     
     idx <- order(rng_start)
     rng_start <- rng_start[idx]
     rng_end <- rng_end[idx]
-    cvalues <- cvalues[idx]
+
+	if (!plotType %in% "seg_tracks") {
+    	cvalues <- cvalues[idx]
+	}
     
+	# create a new plot
     if (verbose) cat("\n plotting...", chrom, "values\n")
     if (newPlot) {
       plotDefaults <- list(
         axes=TRUE, ann=FALSE,
-        xpd=FALSE,                 # clip to plotting region
+        xpd=FALSE,          # clip to plotting region
         xaxs="i", yaxs="i", # let R decide what works
         main="",
         xlab="", ylab="",x=0,y=0,type='n', mgp=c(3,1,0)
@@ -210,58 +303,86 @@ plotType='rect', col='gray50',val_range=NULL,  value_cols="values",
       args <- modifyList(plotDefaults,list(...)); 
       rm(plotDefaults) #overrides default values with user inputs
     }
+
     if (smoothVals) {
       tmp <- loess(y~x,data=list(x=rng_start, y=cvalues), span=span)
       cvalues <- predict(tmp)
       rm(tmp)
     }
+
+	# add data series: vertical
     if (vertical) {
         if (newPlot) {
-        args <- modifyList(args, list(ylim=rev(bpLim), xlim=val_range,
-            yaxt='n',xaxt="n",xpd=NA))
-        if (any(grep("ylab",names(args)))) { 
-            ax_lab <- args$ylab; args$ylab=NULL 
-        } else {
-            ax_lab <- NULL
-        }
-        suppressWarnings(do.call('plot', args))
-        suppressWarnings(axis(side=3,...))
-        if (!is.null(ax_lab)) mtext(ax_lab, side=3, line=2,
-            outer=FALSE,cex=0.8)
+        	args <- modifyList(args, 
+				list(ylim=rev(bpLim), xlim=val_range,
+            		yaxt='n',xaxt="n",xpd=NA))
+        	if (any(grep("ylab",names(args)))) { 
+            	ax_lab <- args$ylab; args$ylab=NULL 
+        	} else {
+           		ax_lab <- NULL
+        	}
+        
+			suppressWarnings(do.call('plot', args))
+        	suppressWarnings(axis(side=3,...))
+        	if (!is.null(ax_lab)) mtext(ax_lab, side=3, line=2,
+            	outer=FALSE,cex=0.8)
         }
         
         if (plotType=="rect") {
-        na_idx <- which(is.na(cvalues))
-        if (any(na_idx)) {
-         suppressWarnings(rect(ybottom=rng_start[-na_idx],
-            ytop=rng_end[-na_idx],xleft=0, 
-            xright=cvalues[-na_idx], border=NA,col=col,...))
-        } else {
-         suppressWarnings(rect(ybottom=rng_start,ytop=rng_end,xleft=0, 
-            xright=cvalues, border=NA,col=col,...))
-         }
+        	na_idx <- which(is.na(cvalues))
+        	if (any(na_idx)) {
+         		suppressWarnings(rect(ybottom=rng_start[-na_idx],
+           			ytop=rng_end[-na_idx],xleft=0, 
+            		xright=cvalues[-na_idx], border=NA,col=col,...))
+        	} else {
+         		suppressWarnings(rect(ybottom=rng_start,ytop=rng_end,
+					xleft=0,xright=cvalues, border=NA,col=col,...))
+         	}
         } else if (plotType=="lines"){
-            suppressWarnings(lines(y=rng_start, x=cvalues, col=col,...))
-        } else {stop('invalid plotType') }
+            suppressWarnings(lines(y=rng_start, 
+								   x=cvalues, col=col,...))
+        } else if (plotType == "seg_tracks") {
+			cat("at seg_tracks\n")
+						browser()	
+		} else {
+			stop('invalid plotType')
+		}
         
         #rect(xleft=cvalues,xright=0,ybottom=rng_start, ytop=rng_end, 
         #    border=NA,...)
         #lines(y=bpLim,x=c(0,0),col='black',lty=1)
         
-    ### Horizontal view
+    # add data series: horizontal
     } else{
-    if (newPlot) {
-        args <- modifyList(args, list(xlim=bpLim,ylim=val_range,
-            xaxt='n',...))
+    	if (newPlot) {
+        	args <- modifyList(args, list(xlim=bpLim,ylim=val_range,
+            	xaxt='n',...))
         suppressWarnings(do.call('plot', args))
-    }
+    	}
     
-    if (plotType=="rect") {
-        suppressWarnings(rect(xleft=rng_start,xright=rng_end,
-            ybottom=0, ytop=cvalues, border=NA,col=col, ...))
-    } else if (plotType=="lines"){
-        suppressWarnings(lines(x=rng_start, y=cvalues, col=col, ...))
-    } else {stop('invalid plotType') }
+	    if (plotType=="rect") {
+	        suppressWarnings(rect(xleft=rng_start,xright=rng_end,
+	            ybottom=0, ytop=cvalues, border=NA,col=col, ...))
+	    } else if (plotType=="lines"){
+	        suppressWarnings(lines(x=rng_start, y=cvalues, 
+								   col=col, ...))
+	    } else if (plotType == "seg_tracks") {
+			cat("horiz:at seg_tracks\n")
+		# plot each track
+			for (k in 1:length(values_GR)) {
+				idx <- k %% length(col)
+				if (idx == 0) idx <- length(col)
+
+				cury <- c(k-0.9,k-0.5)
+				df <- as.data.frame(values_GR[[k]])
+				rect(ybottom=rep(cury[1],nrow(df)),
+					 ytop=rep(cury[2],nrow(df)),
+					 xleft=df$start, xright=df$end,
+					 border=NA,col=col[idx])
+			}
+		} else {
+			stop('invalid plotType') 
+		}
         lines(x=bpLim,y=c(0,0),col='black',lty=1)
     }
     
